cedeb2d17dfb2406b20f8613c41683a4adb886cd
nvim192 <194911047+nvim192@users.noreply.github.com>
2025-01-15 18:14:46 +0100

    Open neo-tree on startup

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -1,6 +1,28 @@
 -- Neo-tree is a Neovim plugin to browse the file system
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
+local function open_neo_tree(data)
+  -- buffer is a [No Name]
+  local no_name = data.file == '' and vim.bo[data.buf].buftype == ''
+
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not no_name and not directory then
+    return
+  end
+
+  -- change to the directory
+  if directory then
+    vim.cmd.cd(data.file)
+  end
+
+  -- open the tree
+  vim.cmd 'Neotree toggle'
+end
+
+vim.api.nvim_create_autocmd({ 'VimEnter' }, { callback = open_neo_tree })
+
 return {
   'nvim-neo-tree/neo-tree.nvim',
   version = '*',
