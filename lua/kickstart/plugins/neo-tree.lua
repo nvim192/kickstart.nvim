e4f25a6628a6fd417ba1bad4b5ccb28dd7fa9faa
nvim192 <194911047+nvim192@users.noreply.github.com>
2025-01-15 18:15:10 +0100

    Show relativenumber on neo-tree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -43,5 +43,15 @@ return {
         },
       },
     },
+    event_handlers = {
+      {
+        event = 'neo_tree_buffer_enter',
+        handler = function(arg)
+          vim.cmd [[
+          setlocal relativenumber
+        ]]
+        end,
+      },
+    },
   },
 }
