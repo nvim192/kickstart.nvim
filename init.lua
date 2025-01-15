bcd1cd171694eab3124c7d35f525278de85c5868
nvim192 <194911047+nvim192@users.noreply.github.com>
2025-01-15 08:56:15 +0100

    Add diagnostic keymaps

--- init.lua
+++ init.lua
@@ -164,7 +164,12 @@ vim.opt.scrolloff = 10
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>de', vim.diagnostic.enable, { desc = 'Enable diagnostics' })
+vim.keymap.set('n', '<leader>dd', function()
+  vim.diagnostic.enable(false)
+end, { desc = 'Disable diagnostics' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
