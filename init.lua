20ed7a594278a6a908f22bfa23a9f3b45ddcdbd3
nvim192 <194911047+nvim192@users.noreply.github.com>
2025-01-15 08:58:31 +0100

    Configure volar lsp server for vue support

--- init.lua
+++ init.lua
@@ -643,6 +643,17 @@ require('lazy').setup({
         -- ts_ls = {},
         --
 
+        volar = {
+          filetypes = { 'vue', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact' },
+          init_options = {
+            vue = {
+              hybridMode = false,
+            },
+            typescript = {
+              tsdk = vim.fn.getcwd() .. '/node_modules/typescript/lib',
+            },
+          },
+        },
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
